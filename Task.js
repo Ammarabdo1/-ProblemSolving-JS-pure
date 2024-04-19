f11d03151a46753e974f3bf602ffac4ab21c1489
AmmarAbdo002 <ammarabbdo.002@gmail.com>
2024-04-20 00:25:42 +0200

    first commit

---
+++ Task.js
@@ -0,0 +1,199 @@
+//! Q1: Create a function to calculate Average to a group of 
+//!     numbers (at least 10 numbers)
+// function Average(){
+//     var arr=[2,5,20,69,39,30,438,39,539,39,49,394,93,1];
+//     var total=0;
+//     for(var i=0; i<arr.length; i++)
+//     {
+//         total+=arr[i];
+//     }
+//     var Average1;
+//     return Average1=total/(i);
+// }
+
+// console.log(Average())
+
+//! Q2: Write a function that takes the base and height of a 
+//!     triangle and return its area
+// function triangle(b,h){
+//     return .5*(b*h);
+// }
+// console.log(triangle(10,5));
+
+//! Q3: Create a function that takes the age in years and
+//!     returns the age in days and print it on console
+// function age(years){
+//     var days=0;
+//     return days= years*12*30;
+// }
+// console.log(age(1))
+
+//! Q4: Create a function takes two numbers and return 
+//!     their sum
+// function sum(x,y){
+//     return x+y;
+// }
+// console.log(sum(2,3));
+
+//! Q5: Create a function that takes an array containing 
+//!     only numbers and return the first element
+// function array(arr){
+//     return arr[0];
+// }
+// var arr=[3,5,2,5];
+// document.write(array(arr));
+
+//! Q6: Create a function show your name in HTML 
+//!     document
+// function myName(){
+//     document.getElementById("name").innerHTML="ammar";
+// }
+// myName();
+
+//! Q7: Write a function that takes an integer hours and 
+//!     converts it to second 
+// function hour(hours){
+//     return hours*60*60;
+// }
+// console.log(hour(1))
+
+//! Q8: Given four numbers, return true if the sum of both 
+//!     numbers is more than 350. Otherwise return false
+// var x=200, y=160;
+// var check;
+// if((x+y)>350){
+//     check=true;
+// }
+// else{
+//     check=false;
+// }
+// console.log(check);
+
+//! Q9:  Write a function that takes a number if number equal zero 
+//!      return true otherwise return false
+// function type(num){
+//     if(num==0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// document.write(type(20));
+
+//! Q10: create a function takes tow number and return 
+// function calc(x,y){
+//     return x%y;
+// }
+// document.getElementById("calc").innerHTML=calc(3,2);
+
+//! Q11: Make a function take two parameters num1 and num2 and print the 
+//!      greater number or if they are equal print they are equal
+// function largest(num1,num2){
+//     if(num1>num2){
+//         return num1+" is a greater number";
+//     }
+//     else if(num1<num2){
+//         return num2+" is a greater number";
+//     }
+//     else{
+//         return "They are equal";
+//     }
+// }
+// alert(largest(3,1))
+
+//! Q12: Check if input variable is a number or not
+// var _type="ammar";
+// var check
+// if(typeof(_type)=="number"){
+//     check="your variable is a number";
+    
+// }
+// else{
+//     check="your variable isn't a number";
+// }
+// document.write(check);
+
+//! Q13: Write a function to get the current date
+// document.write(new Date());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
